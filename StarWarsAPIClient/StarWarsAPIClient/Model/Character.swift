efe62d81c50aa123c2abff64ac99bfab238dccd0
Mitch <mitch@easy-target.eu>
2024-03-20 16:35:09 +0100

    feat: replace sample Item with SW Character
    Refs: #2

---
+++ StarWarsAPIClient/StarWarsAPIClient/Model/Character.swift
@@ -0,0 +1,22 @@
+internal struct Character: Identifiable {
+    let id: Int
+    let name: String
+    let heightM: Float
+    let massKG: Float
+    let gender: String
+    let homeworldName: String
+    let wikiURL: String
+    let imageURL: String
+    let yearOfBirth: Int
+    let placeOfBirthName: String
+    let yearOfDeath: Int
+    let placeOfDeathName: String
+    let speciesName: String
+    let hairColor: String
+    let eyeColor: String
+    let cybernetics: String
+    let affiliationNames: String
+    let formerAffiliationNames: String
+    let masterNames: String
+    let apprenticeNames: String
+}
